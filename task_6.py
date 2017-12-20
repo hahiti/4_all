cb86627433bde2cc581416dd9e7995fc988619ab
Kathrine <comeinskaya@gmail.ru>
2017-12-20 22:08:59 +0200

    initial commit

--- task_6.py
+++ task_6.py
@@ -1,8 +1,13 @@
-print (" ===================== ")
+print ("***********************")
 print ("task #6")
-print (" ===================== ")
+print ("***********************")
 
 a = 10
 b = 2
-c = a/b
-print ("Result of a/b (a=d%d, b=d%d) is %.2f" % (a, b, c))
+
+
+if b!=0:
+    c = a/b
+    print ("Result of a/b (a=d%d, b=d%d) is %.2f" % (a, b, c))
+else:
+    print ("Error: Division by zero!")
